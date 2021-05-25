c4b95ac08c9fb781543914b03ddf895d708d4bcf
KazukiYamauchi <32021@ojs.ac.jp>
2021-05-25 11:06:06 +0900

    P55 リスト2-10 定数を定義(0.3048を定数化)

--- DistanceConverter/FeetConverter.cs
+++ DistanceConverter/FeetConverter.cs
@@ -7,14 +7,15 @@ using System.Threading.Tasks;
 namespace DistanceConverter {
     //フィートとメートルの単位変換クラス
     public static class FeetConverter {//staticのメソッドしかないから、クラス自体をstaticにする。→別クラスでインスタンスを生成(new)できなくなる
+        private const double ratio = 0.3048;
         //メートルからフィートを求める
         public　static double FromMeter(double meter) {
-            return meter / 0.3048;
+            return meter / ratio;
         }
 
         //フィートからメートルを求める
         public static double ToMeter(double feet) {
-            return feet * 0.3048;
+            return feet * ratio;
         }
     }
 }
