d9ecdcfb45ed1fa0decf743193d29904e8e2403c
KazukiYamauchi <32021@ojs.ac.jp>
2021-05-25 11:34:20 +0900

    P59 リスト2-14 売上クラス作成

---
+++ SalesCalculator/Sale.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator {
+    class Sale {
+        //店舗名
+        public string ShopName { get; set; }//自動実装プロパティgetter,setterと同じ
+
+        //商品カテゴリ
+        public string ProductCategory { get; set; }
+
+        //売上高
+        public int Amount { get; set; }
+    }
+}
