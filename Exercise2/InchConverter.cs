b03492e0a7914425509364a8f0228fd1c0c5854d
KazukiYamauchi <32021@ojs.ac.jp>
2021-06-01 10:26:53 +0900

    P79 演習問題2-2

---
+++ Exercise2/InchConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    //インチとメートルの単位変換クラス
+    public static class InchConverter {//staticのメソッドしかないから、クラス自体をstaticにする。→別クラスでインスタンスを生成(new)できなくなる
+        private const double ratio = 0.0254;
+        //メートルからインチを求める
+        public　static double FromMeter(double meter) {
+            return meter / ratio;
+        }
+
+        //インチからメートルを求める
+        public static double ToMeter(double feet) {
+            return feet * ratio;
+        }
+    }
+}
---
+++ Exercise2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length >= 1 && args[0] == "-toi") {
+                //メートルからインチへの対応表を出力
+                PrintMeterToInchList(1, 10);
+            } else {
+                //インチからメートルへの対応表を出力
+                PrintInchToMeterList(1, 10);
+            }
+        }
+        //インチからメートルへの対応表を出力
+        private static void PrintMeterToInchList(int start,int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double inch = InchConverter.FromMeter(meter);//静的メソッドだから、クラス名でアクセス
+                Console.WriteLine("{0}m = {1:0.0000}inch", meter, inch);
+            }
+
+        }
+        //インチからメートルへの対応表を出力
+        private static void PrintInchToMeterList(int start,int stop) {
+            for (int inch = start; inch <= stop; inch++) {
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine("{0}inch = {1:0.0000}m", inch, meter);
+            }
+        }
+    }
+}
+
