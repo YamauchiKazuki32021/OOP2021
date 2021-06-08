e4df1d796e97792223628d1883986e91c4555232
KazukiYamauchi <32021@ojs.ac.jp>
2021-06-08 09:31:32 +0900

    P100 演習問題

---
+++ Section01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            var sc = new SampleCode();
+            Console.WriteLine(sc.Count(5));
+        }
+    }
+    class SampleCode {
+        public int Count(int num) {
+            var numbers = new[] { 5,3,9,6,7,5,8,1,0,5,10,4};
+            int count = 0;
+            foreach(var n in numbers) {
+                if (n == num)
+                    count++;
+            }
+            return count;
+        }
+    }
+
+    
+
+}
+
