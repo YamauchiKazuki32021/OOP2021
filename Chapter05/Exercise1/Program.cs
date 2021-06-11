a13fbd75177685670962cc747a98bb2898f99d6a
KazukiYamauchi <32021@ojs.ac.jp>
2021-06-11 15:52:59 +0900

    P158 演習問題5-1

---
+++ Chapter05/Exercise1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+#if false
+            #region 問題5.1
+            var s1 = Console.ReadLine();
+            var s2 = Console.ReadLine();
+
+            if (string.Compare(s1, s2, ignoreCase:true) == 0) { 
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+            #endregion
+#endif
+        }
+    }
+}
