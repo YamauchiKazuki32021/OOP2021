331dce88ae8cefd0a893e02ebd3c5c790d8412ef
KazukiYamauchi <32021@ojs.ac.jp>
2021-07-01 15:16:11 +0900

    演習問題６終了

---
+++ Chapter06/Exercise/Exercise1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+            #region テスト用ドライバ
+            Exercise1_1(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_2(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_4(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_5(numbers);
+            #endregion
+        }
+
+        private static void Exercise1_1(int[] numbers) {
+            Console.WriteLine(numbers.Max());
+        }
+
+        private static void Exercise1_2(int[] numbers) {
+            var skip = numbers.Length - 2;
+            foreach (var n in numbers.Skip(skip))
+                Console.WriteLine(n);
+
+            
+        }
+
+        private static void Exercise1_3(int[] numbers) {
+            foreach(var n in numbers.Select(n=> n.ToString()))
+            Console.WriteLine(n);
+        }
+
+        private static void Exercise1_4(int[] numbers) {
+            foreach (var n in numbers.OrderBy(n => n).Take(3))
+                Console.WriteLine(n);
+            
+        }
+
+        private static void Exercise1_5(int[] numbers) {
+            var count = numbers.Distinct().Count(n => n > 10);
+            Console.WriteLine(count);
+        }
+    }
+}
--- Chapter06/Section01/Program.cs
+++ Chapter06/Section01/Program.cs
@@ -7,6 +7,32 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
+            var books = Books.GetBooks();
+
+            Console.WriteLine("本の平均価格は、" + books.Average(x => x.Price).ToString("#,0") + "円");
+            Console.WriteLine("ページ数の合計は、" + books.Sum(x => x.Pages) + "ページ");
+            Console.WriteLine("最も価格の高い本は、" + books.Max(x => x.Price) + "円");
+            Console.WriteLine("最も価格の安い本は、" + books.Min(x => x.Price) + "円");
+            Console.WriteLine("500円以上の本は、" + books.Where(x => 500 <= x.Price).Count() + "冊");
+            Console.WriteLine("「物語」が含まれている本は、" + books.Count(x=> x.Title.Contains("物語")) + "冊");
+            var bookData = books.Where(x => x.Title.Contains("物語"));
+            foreach(var book in bookData) {
+                Console.WriteLine(book.Title);
+                
+            }
+            Console.WriteLine();
+
+            //高額書籍ベスト３
+            var books1 = books.OrderByDescending(x => x.Price).Take(3);
+            foreach(var item in books1) {
+                Console.WriteLine(item.Title+("、") + item.Price+("円"));
+            }
+            Console.WriteLine();
+
+            var titles = books.Select(x => x.Title);
+            foreach (var item in titles) {
+                Console.WriteLine(item);
+            }
         }
     }
 }
