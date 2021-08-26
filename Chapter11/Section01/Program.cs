8ef9f27e7c8b2104c15e77f0132320dbd94a638c
山内 一叶 <32021@ojs.ac.jp>
2021-08-26 15:39:37 +0900

    リスト11.7まで

---
+++ Chapter11/Section01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            var xdoc = XDocument.Load("novelists.xml");
+            //var xelements = xdoc.Root.Elements().Where(x => ((DateTime)x.Element("birth")).Year >= 1900);
+            //var xelements = xdoc.Root.Elements().OrderBy(x => (string)(x.Element("name").Attribute("kana")));
+
+            foreach (var xnovelist in xdoc.Root.Elements()) {
+                var xname = xnovelist.Element("name");
+                var works = xnovelist.Element("masterpieces").Elements("title").Select(x => x.Value);
+                //var xbirth = (DateTime)xnovelist.Element("birth");
+                //XAttribute xkana = xname.Attribute("kana");
+                Console.WriteLine("{0}-{1}",xname.Value, string.Join(",",works));
+            }
+        }
+    }
+}
