d7ce4dff77b29576eeb0a14acd7526b34e8752c9
KazukiYamauchi <32021@ojs.ac.jp>
2021-08-26 13:21:50 +0900

    MathクラスのPowメソッドを使用

--- Sample0823/Form1.cs
+++ Sample0823/Form1.cs
@@ -17,11 +17,13 @@ namespace Sample0823 {
         private void Exec_Click(object sender, EventArgs e) {
             int num1 = int.Parse(Value.Text);
             int num2 = int.Parse(Jyou.Text);
-            int num3 =1;
-            for (int i=1;i<=num2;i++) {
-                  num3 *= num1;
-            }
-            Result.Text = num3.ToString();
+            //int num3 =1;
+            //for (int i=1;i<=num2;i++) {
+            //      num3 *= num1;
+            //}
+            //Result.Text = num3.ToString();
+
+            Result.Text = (Math.Pow((double)num1, (double)num2)).ToString();
         }
     }
 }
