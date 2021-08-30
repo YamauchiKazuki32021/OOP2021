3c77d6a57f58dfe56f720c4f2d1622ce6874c56a
山内 一叶 <32021@ojs.ac.jp>
2021-08-30 16:01:05 +0900

    20210830_16:00

---
+++ Chapter11/Exercise2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Exercise2 {
+    class Program {
+        static void Main(string[] args) {
+            var file = "11_2.xml";
+            var xdoc = XDocument.Load(file);
+        }
+    }
+}
