ea160ecea14db2b4a35095d93b7790e550d6befa
山内 一叶 <32021@ojs.ac.jp>
2021-09-06 14:02:00 +0900

    演習問題完成

--- Chapter14/Section04/Program.cs
+++ Chapter14/Section04/Program.cs
@@ -10,52 +10,89 @@ using System.Xml.Linq;
 
 namespace Section04 {
     class Program {
+        Dictionary<string, int> AreaDic = new Dictionary<string, int>() {
+                {"前橋",4210 },
+                {"みなかみ",4220 },
+                {"宇都宮",4110 },
+                {"水戸",4010 }
+            };
+        //コードを保存する
+        List<int> cityCode = new List<int>();
+
         static void Main(string[] args) {
             new Program();
         }
         
         //コンストラクタ
         public Program() {
+ 
             //DownloadString();
             //OpenReadSample();
             Console.WriteLine("Yahoo週間天気予報");
             Console.WriteLine("地域コードを入力");
-            Console.WriteLine("1:前橋");//4210
-            Console.WriteLine("2:みなかみ");//4220
-            Console.WriteLine("3:宇都宮");//4110
-            Console.WriteLine("4:水戸");//4010
+            //Console.WriteLine("1:前橋");//4210
+            //Console.WriteLine("2:みなかみ");//4220
+            //Console.WriteLine("3:宇都宮");//4110
+            //Console.WriteLine("4:水戸");//4010
+           
+            
+            int num = 1;
+
+            foreach (KeyValuePair<string,int> pair in AreaDic) {
+                Console.WriteLine("{0}:{1}",num++,pair.Key);
+                cityCode.Add(pair.Value);//コードをリストへ保存
+            }
+
             Console.WriteLine("9:その他(直接入力)");//任意のコード
             Console.WriteLine();
             Console.Write(">");
-            var i = int.Parse(Console.ReadLine());
-            int code = 0;
-            switch (i) {
-                case 1:
-                    code = 4210;
-                    break;
-                case 2:
-                    code = 4220;
-                    break;
-                case 3:
-                    code = 4110;
-                    break;
-                case 4:
-                    code = 4010;
-                    break;
-                case 9:
-                    Console.WriteLine("地域コードを入力");
-                    code = int.Parse(Console.ReadLine());
-                    break;
 
-            }
-
+            var selectArea = Console.ReadLine();
+            int pos = int.Parse(selectArea);
             
-           var results = GetWeatherReportFromYahoo(code);
-
-
+            int code;
+            if (pos != 9) {
+                 code = cityCode[pos - 1];
+            } else {
+                //その他の場合
+                Console.Write("都市コードを入力");
+                var inputCode = Console.ReadLine();
+                int.Parse(inputCode);
+                 code = int.Parse(inputCode);
+            }
+            var results = GetWeatherReportFromYahoo(code);
             foreach (var s in results) {
                 Console.WriteLine(s);
             }
+           // var i = int.Parse(Console.ReadLine());
+           // int code = 0;
+           // switch (i) {
+           //     case 1:
+           //         code = 4210;
+           //         break;
+           //     case 2:
+           //         code = 4220;
+           //         break;
+           //     case 3:
+           //         code = 4110;
+           //         break;
+           //     case 4:
+           //         code = 4010;
+           //         break;
+           //     case 9:
+           //         Console.WriteLine("地域コードを入力");
+           //         code = int.Parse(Console.ReadLine());
+           //         break;
+
+           // }
+
+            
+           //var results = GetWeatherReportFromYahoo(code);
+
+
+            //foreach (var s in results) {
+            //    Console.WriteLine(s);
+            //}
             //Console.ReadLine();//入力待ち
         }
         //リスト14.15
