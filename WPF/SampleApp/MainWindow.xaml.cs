ac1701dd85622f5d7b07d0ed72984f1da6303f48
山内 一叶 <32021@ojs.ac.jp>
2021-11-05 15:57:12 +0900

    WPFサンプルアプリ

---
+++ WPF/SampleApp/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SampleApp {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            //btHello.Content = "ボタンが押されました。";
+            //MessageBox.Show("ボタンが押されました。");
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e) {
+            MessageBox.Show(string.Format("入力された文字は{0}です。",messageTextBox.Text));
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e) {
+            MessageBox.Show("キャンセルされました。");
+        }
+    }
+}
