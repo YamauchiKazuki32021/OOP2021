e7081efde851dc5f4dae169670c804f9f2ca0abe
山内 一叶 <32021@ojs.ac.jp>
2021-11-10 15:31:44 +0900

    2021/11/10

---
+++ WPF/SampleUnitConverter/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SampleUnitConverter {
+    public class MainWindowViewModel : ViewModel {
+        private double metricValue;
+        private double imperialValue;
+
+        //▲ボタンで呼ばれるコマンド(ヤード単位からメートル単位)
+        public ICommand ImperialUnitToMetric { get; private set; }
+        //▼ボタンで呼ばれるコマンド(メートル単位からヤード単位)
+        public ICommand MetricToImperialUnit { get; private set; }
+
+        //上のComboBoxで選択されている値
+        //public MetricUnit CurrentMetricUnit { get; set; }
+        //下のComboBoxで選択されている値
+        //public ImperialUnit CurrentImperialUnit { get; set; }
+
+        public double MetricValue {
+            get { return this.metricValue; }
+            set {
+                this.metricValue = value; 
+            }
+        }
+    }
+}
