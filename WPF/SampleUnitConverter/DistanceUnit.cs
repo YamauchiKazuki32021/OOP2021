35712de65d9dd6c1bd0341e0a47bc5e85704729d
山内 一叶 <32021@ojs.ac.jp>
2021-11-12 13:49:11 +0900

    SampleUnitConverter完成

--- WPF/SampleUnitConverter/DistanceUnit.cs
+++ WPF/SampleUnitConverter/DistanceUnit.cs
@@ -21,15 +21,33 @@ namespace SampleUnitConverter {
             new MetricUnit { Name ="km",Coefficient=10 * 100 * 1000,},
         };
         public static ICollection<MetricUnit> Units { get { return units; } }
-        //ヤード単位を表すクラス
-        public class ImperialUnit : DistanceUnit {
-            private static List<ImperialUnit> units = new List<ImperialUnit> {
+        /// <summary>
+        /// ヤード単位からメートル単位に変換します。
+        /// </summary>
+        /// <param name="unit">ヤード単位</param>
+        /// <param name="value">係数</param>
+        /// <returns></returns>
+        public double FromImperialUnit(ImperialUnit unit, double value) {
+            return(value * unit.Coefficient) * 25.4 /this.Coefficient;
+        }
+    }
+        //ヤード単位を表すクラス   
+    public class ImperialUnit : DistanceUnit {
+        private static List<ImperialUnit> units = new List<ImperialUnit> {
             new ImperialUnit { Name ="in",Coefficient=1,},
             new ImperialUnit { Name ="ft",Coefficient=12,},
             new ImperialUnit { Name ="yd",Coefficient=12 * 3,},
             new ImperialUnit { Name ="ml",Coefficient=12 * 3 * 1760,},
             };
-            public static ICollection<ImperialUnit> Units { get { return units; } }
+        public static ICollection<ImperialUnit> Units { get { return units; } }
+        /// <summary>
+        /// メートル単位からヤード単位に変換します。
+        /// </summary>
+        /// <param name="unit">メートル単位</param>
+        /// <param name="value">係数</param>
+        /// <returns></returns>
+        public double FromMetricUnit(MetricUnit unit, double value) {
+            return (value * unit.Coefficient) / 25.4 / this.Coefficient;
         }
     }
 }
--- WPF/SampleUnitConverter/MainWindowViewModel.cs
+++ WPF/SampleUnitConverter/MainWindowViewModel.cs
@@ -17,9 +17,9 @@ namespace SampleUnitConverter {
         public ICommand MetricToImperialUnit { get; private set; }
 
         //上のComboBoxで選択されている値
-        //public MetricUnit CurrentMetricUnit { get; set; }
+        public MetricUnit CurrentMetricUnit { get; set; }
         //下のComboBoxで選択されている値
-        //public ImperialUnit CurrentImperialUnit { get; set; }
+        public ImperialUnit CurrentImperialUnit { get; set; }
 
         public double MetricValue {
             get { return this.metricValue; }
@@ -35,6 +35,18 @@ namespace SampleUnitConverter {
                 this.OnPropertyChanged();//Viewへ通知
             }
         }
+        //コンストラクタ
+        public MainWindowViewModel() {
+            this.CurrentMetricUnit = MetricUnit.Units.First();
+            this.CurrentImperialUnit = ImperialUnit.Units.First();
 
+            this.MetricToImperialUnit = new DelegateCommand(
+                () => this.ImperialValue = this.CurrentImperialUnit.FromMetricUnit(this.CurrentMetricUnit, this.MetricValue)
+                );
+
+            this.ImperialUnitToMetric = new DelegateCommand(
+    () => this.MetricValue = this.CurrentMetricUnit.FromImperialUnit(this.CurrentImperialUnit, this.ImperialValue)
+    );
+        }
     }
 }
