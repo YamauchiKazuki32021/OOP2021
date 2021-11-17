b14efa5a6d19fb3bd0f16a59d106df45a0d554b6
山内 一叶 <32021@ojs.ac.jp>
2021-11-17 14:19:46 +0900

    NumberGame完成

--- WPF/NumberGame/MainWindow.xaml.cs
+++ WPF/NumberGame/MainWindow.xaml.cs
@@ -18,25 +18,28 @@ namespace NumberGame {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
-        Random random = new Random();
+       static Random random = new Random();
+        int answer =int.Parse(random.Next(1, 25).ToString());
         public MainWindow() {
             InitializeComponent();
             
-            random.Next(1,25);
+            //random.Next(1,25);
 
         }
         
         private void Button_Click(object sender, RoutedEventArgs e) {
             Anser(int.Parse(((Button) sender).Content.ToString()));
+            
         }
 
         private void Anser(int number) {
-            if (number.Equals(random)) {
+            if (number==answer) {
                 tbAns.Text = "正解。";
             } else {
-                if(number > int.Parse(random).ToString()) {
-
-
+                if(number > answer) {
+                    tbAns.Text = "この数値より小さいです。";
+                } else {
+                    tbAns.Text = "この数値より大きいです。";
                 }
             }
         }
