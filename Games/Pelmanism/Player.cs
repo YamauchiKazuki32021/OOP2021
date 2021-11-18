8acdec8be2be7dd21f2c700a5d985d25adc47747
山内 一叶 <32021@ojs.ac.jp>
2021-11-18 11:14:54 +0900

    2021/11/18

---
+++ Games/Pelmanism/Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pelmanism {
+    class Player {
+        private int nowOpenCardIndex1;//今回1枚目に開いたカードの添え字
+        private int nowOpenCardIndex2;//今回2枚目に開いたカードの添え字
+
+        //前回1枚目に開いたカードの添え字
+        public int BeforeOpenCardIndex1 { get; set; }
+        //前回2枚目に開いたカードの添え字
+        public int BeforeOpenCardIndex2 { get; set; }
+
+        //めくった枚数
+        public int OpenCounter { get; set; }
+
+        //コンストラクタ
+        public Player() {
+            NowOpenCardIndex1 = NowOpenCardIndex2 = -1;
+            BeforeOpenCardIndex1 = BeforeOpenCardIndex2 = -1;
+            OpenCounter = 0;
+        }
+
+        //今回1枚目に開いたカードの添え字
+        public int NowOpenCardIndex1 {
+            get { return nowOpenCardIndex1; }
+            set {
+                nowOpenCardIndex1 = value;
+                OpenCounter++;
+            }
+        }
+
+        public int NowOpenCardIndex2 {
+            get { return nowOpenCardIndex2; }
+            set {
+                nowOpenCardIndex2 = value;
+                OpenCounter++;
+            }
+        }
+
+        //カードの情報をリセットする。
+        public void Reset() {
+            BeforeOpenCardIndex1 = NowOpenCardIndex1;
+            BeforeOpenCardIndex2 = NowOpenCardIndex2;
+            NowOpenCardIndex1 = -1;
+            NowOpenCardIndex2 = -1;
+            OpenCounter = 0;
+        }
+    }
+}
--- WPF/NumberGame/MainWindow.xaml.cs
+++ WPF/NumberGame/MainWindow.xaml.cs
@@ -19,26 +19,67 @@ namespace NumberGame {
     /// </summary>
     public partial class MainWindow : Window {
        static Random random = new Random();
-        int answer =int.Parse(random.Next(1, 25).ToString());
+        //int answer =int.Parse(random.Next(1, 25).ToString());
+        int answer;
+        private const int Rows = 7;//行
+        private const int Cols = 7;//列
         public MainWindow() {
             InitializeComponent();
             
-            //random.Next(1,25);
+            
 
         }
         
         private void Button_Click(object sender, RoutedEventArgs e) {
-            Anser(int.Parse(((Button) sender).Content.ToString()));
-            
+           
         }
+        private void MainDisp_Loaded(object sender, RoutedEventArgs e) {
+            
+                List<Button> buttons = new List<Button>();
+                //正解を取得
+                answer = random.Next(Rows * Cols) + 1;
 
-        private void Anser(int number) {
-            if (number==answer) {
+                //行
+                for (int i = 0; i < Rows; i++) {
+                    grid.RowDefinitions.Add(new RowDefinition());
+                }
+                //列
+                for (int j = 0; j < Cols; j++) {
+                    grid.ColumnDefinitions.Add(new ColumnDefinition());
+                }
+            int a = 0;
+                for (int i = 0; i < Rows; i++) {
+                    for (int j = 0; j < Cols; j++) {
+                        var bt = new Button();
+                        bt.Width = MainForm.Width / Cols;
+                        bt.Height = MainForm.Height / Rows;
+                        bt.Content = a + 1;
+                        bt.FontSize = 20;
+                        bt.Click += Bt_Click;
+                        Grid.SetRow(bt, i);
+                        Grid.SetColumn(bt, j);
+                        buttons.Add(bt);
+                    a++;
+                    }
+                }
+                buttons.ForEach(bt => grid.Children.Add(bt));
+                MainForm.Height += tbAns.Height + 50;
+
+        }
+            private void Bt_Click(object sender, RoutedEventArgs e) {
+            Button selectedButton = (Button)sender;
+            int num = (int)selectedButton.Content;
+            var ans = (int.Parse(((Button)sender).Content.ToString()));
+
+            if (ans == answer) {
+                ((Button)sender).Background = Brushes.Red;
                 tbAns.Text = "正解。";
             } else {
-                if(number > answer) {
+                if (ans > answer) {
+                    ((Button)sender).Background = Brushes.Yellow;
                     tbAns.Text = "この数値より小さいです。";
                 } else {
+                    ((Button)sender).Background = Brushes.Yellow;
                     tbAns.Text = "この数値より大きいです。";
                 }
             }
